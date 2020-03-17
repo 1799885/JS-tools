b87bc57f366eb1cf0c5d9b8d99b14098cb812b33
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-17 19:08:29 +0100

    added option to round number matrices

--- matrix/matrixRepresentation.js
+++ matrix/matrixRepresentation.js
@@ -1,20 +1,37 @@
-function printMatrix_nD(m, delimeter){
-    console.log(matrixToString(m, delimeter));
+function printMatrix_nD(m, delimeter, round){
+    console.log(matrixToString(m, delimeter, round));
 }
-function matrixToString(m, delimeter){
-    let d;
-    let str = "[";
-    for(let i = 0; i < m.length; i++){
-        if(Array.isArray(m[i])){
-            str += matrixToString(m[i], delimeter);
-            d = (delimeter)? delimeter : "\n";
+function matrixToString(m, delimeter, round){
+    if(round == true){
+        let copy = matrix.make.copy(m);
+        for(let i = 0; i < m.length; i++){
+            for(let j = 0; j < m[0].length; j++){
+                copy[i][j] = Math.round(m[i][j]);
+            }
         }
-        else{
-            str += m[i];
-            d = ",";
-        }
-        str += ((i + 1 < m.length) ?  d + " " : "");
+        m = copy;
+        return matrixToString(copy, delimeter);
     }
-    str += "]";
-    return str;
+    else{
+        let d;
+        let str = "[";
+        for(let i = 0; i < m.length; i++){
+            if(Array.isArray(m[i])){
+                str += matrixToString(m[i], delimeter);
+                d = (delimeter)? delimeter : "\n";
+            }
+            else{
+                str += m[i];
+                d = ",";
+            }
+            str += ((i + 1 < m.length) ?  d + " " : "");
+        }
+        str += "]";
+        return str;
+    }
 }
+/*
+TO DO:
+
+-add JSDOC
+*/
