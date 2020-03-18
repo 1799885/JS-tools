5c58594105953564bb7a2c0d681436a0fcd9a6f7
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-19 00:27:01 +0100

    size, getSlice and SetSlice added

--- matrix/array_nD.js
+++ matrix/array_nD.js
@@ -7,12 +7,125 @@ array_nD = {
                     return matrix.make.empty(dims[0], dims[1]);
                 }
                 for(let i = 0; i < dims[0]; i++){
-                    arr.push(array_nD.make.empty(dims.slice(1)));
+                    arr.push(array_nD.make.empty(...dims.slice(1)));
                 }
                 return arr;
             }
             catch(error){
                 console.log(error);
+                return null;
+            }
+        },
+        copy: function(m){
+            let c = [];
+            let dim = matrix.p.size(m);
+            for(i=0; i < dim.x; i++){
+                c.push([]);
+                for(j = 0; j < dim.y; j++){
+                    c[i][j] = m[i][j];
+                }
+            }
+            return c;
+        }
+    },
+    p: {
+        size: function(arr){
+            let dim = [];
+            if(Array.isArray(arr[0])){
+                dim.unshift(...array_nD.p.size(arr[0]));
+                dim.unshift(arr.length);
+            }
+            else{
+                dim.unshift(arr.length);
+            }
+            return dim;
+        }
+    },
+    o: {
+        permutation_3D: function(arr3D, axis, h){
+            try{
+                switch(true){
+                    case vector.re.X.test(axis)://x = cte = h
+
+                        break;
+                    case vector.re.Y.test(axis)://y = cte = h
+
+                        break;
+                    case vector.re.Z.test(axis)://z = cte = h
+
+                        break;
+                }
+            }
+            catch(error){
+                console.log(error);
+                return null;
+            }
+        },
+        get3DSlice: function(arr3D, axis, h){
+            try{
+                let m = matrix.make.empty(3,3);
+                switch(true){
+                    case vector.re.X.test(axis)://x = cte = h
+                        for(let j = 0; j < 3; j++){
+                            for(let k = 0; k < 3; k++){
+                                m[j][k] = arr3D[h][j][k];
+                            }
+                        }
+                        break;
+                    case vector.re.Y.test(axis)://y = cte = h
+                        for(let i = 0; i < 3; i++){
+                            for(let k = 0; k < 3; k++){
+                                m[i][k] = arr3D[i][h][k];
+                            }
+                        }
+                        break;
+                    case vector.re.Z.test(axis)://z = cte = h
+                        for(let i = 0; i < 3; i++){
+                            for(let j = 0; j < 3; j++){
+                                m[i][j] = arr3D[i][j][h];
+                            }
+                        }
+                        break;
+                }
+                // printMatrix_nD(m);
+                return m;
+            }
+            catch(error){
+                console.log(error);
+                return null;
+            }
+        },
+        set3DSlice: function(arr3D, axis, h, slice){
+            try{
+                switch(true){
+                    case vector.re.X.test(axis)://x = cte = h
+                        for(let j = 0; j < 3; j++){
+                            for(let k = 0; k < 3; k++){
+                                arr3D[h][j][k] = slice[j][k];
+                            }
+                        }
+                        break;
+                    case vector.re.Y.test(axis)://y = cte = h
+                        for(let i = 0; i < 3; i++){
+                            for(let k = 0; k < 3; k++){
+                                arr3D[i][h][k] = slice[i][k];
+                            }
+                        }
+                        break;
+                    case vector.re.Z.test(axis)://z = cte = h
+                        for(let i = 0; i < 3; i++){
+                            for(let j = 0; j < 3; j++){
+                                arr3D[i][j][h] = slice[i][j];
+                            }
+                        }
+                        break;
+                }
+                printMatrix_nD(arr3D);
+                return arr3D;
+            }
+            catch(error){
+                console.log(error);
+                return null;
             }
         }
     }
