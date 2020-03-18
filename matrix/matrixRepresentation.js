e618920366d38db9aba4270f0acae18c408c0979
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-18 23:05:11 +0100

    added espace in printArray_nD to print matrices of more than 3 dimensions

--- matrix/matrixRepresentation.js
+++ matrix/matrixRepresentation.js
@@ -2,7 +2,7 @@ function printMatrix_nD(m, delimeter, round){
     console.log(matrixToString(m, delimeter, round));
 }
 function matrixToString(m, delimeter, round){
-    if(round == true){
+    if(round == true){ //remove the "true" when jsdoc done
         let copy = matrix.make.copy(m);
         for(let i = 0; i < m.length; i++){
             for(let j = 0; j < m[0].length; j++){
@@ -13,10 +13,13 @@ function matrixToString(m, delimeter, round){
         return matrixToString(copy, delimeter);
     }
     else{
-        let d;
+        let d, end = "";
         let str = "[";
         for(let i = 0; i < m.length; i++){
             if(Array.isArray(m[i])){
+                if(Array.isArray(m[i][0])){
+                    end = "\n";
+                }
                 str += matrixToString(m[i], delimeter);
                 d = (delimeter)? delimeter : "\n";
             }
@@ -24,7 +27,7 @@ function matrixToString(m, delimeter, round){
                 str += m[i];
                 d = ",";
             }
-            str += ((i + 1 < m.length) ?  d + " " : "");
+            str += ((i + 1 < m.length) ?  d + " " : "") + end;
         }
         str += "]";
         return str;
