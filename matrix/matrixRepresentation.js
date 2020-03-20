cecba1fb5ddf566fc12ae3ef2dae193ed13dd041
Jkutkut <jorgeregonzalez@gmail.com>
2020-03-21 00:14:09 +0100

    JSDOC added. Stable version

--- matrix/matrixRepresentation.js
+++ matrix/matrixRepresentation.js
@@ -1,10 +1,26 @@
+/**
+ * Different way of calling printArray_nD function.
+ * @see {function} printArray_nD.
+ * @deprecated
+ */
 function printMatrix_nD(m, delimeter, round){
-    console.log(matrixToString(m, delimeter, round));
+    printArray_nD(m, delimeter, round);
 }
+/**
+ * Function that prints the result of running the function array_nDToString.
+ * @see {function} array_nDToString.
+ */
 function printArray_nD(m, delimeter, round){
-    console.log(matrixToString(m, delimeter, round));
+    console.log(array_nDToString(m, delimeter, round));
 }
-function matrixToString(m, delimeter, round){
+/**
+ * Way to get the toString() method of a array of (arrays of...) of order n.
+ * @param {array[]} m - array of (arrays of...) of order n. 
+ * @param {string} delimeter - delimiter to separate the elements of m.
+ * @param {boolean} round - If the elements of m should be printed using Math.round().
+ * @returs {string} the string representation of the matrix m.
+*/
+function array_nDToString(m, delimeter, round){
     if(round == true){ //remove the "true" when jsdoc done
         let copy = matrix.make.copy(m);
         for(let i = 0; i < m.length; i++){
